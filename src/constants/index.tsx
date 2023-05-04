b9d5e38741d18e229061e85702176777b397c6cd
Ksechan <rlatpcks77@gmail.com>
2023-05-04 18:51:44 +0900

    feat: add projectList array

---
+++ src/constants/index.tsx
@@ -0,0 +1,33 @@
+export const projectList = [
+  { id: 0, title: 'sechan', description: '프론트엔드 김세찬입니다.' },
+  {
+    id: 1,
+    title: 'hanwha-motiev',
+    description: '한화모티브입니다.',
+  },
+  {
+    id: 2,
+    title: 'pantos',
+    description: '판토스입니다.',
+  },
+  {
+    id: 3,
+    title: 'convatec',
+    description: '콘바텍입니다.',
+  },
+  {
+    id: 4,
+    title: 'school-trip',
+    description: '스쿨트립입니다.',
+  },
+  {
+    id: 5,
+    title: 'hubbing',
+    description: '허빙입니다.',
+  },
+  {
+    id: 6,
+    title: 'alba-edu',
+    description: '알바에듀입니다.',
+  },
+];
