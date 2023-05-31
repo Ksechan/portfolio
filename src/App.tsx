4c421478ef75d9b3b37781ab9a2512276a0a55a7
Ksechan <rlatpcks77@gmail.com>
2023-05-31 14:36:58 +0900

    feat: add projectPage router

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { GlobalStyle } from './components/GlobalStyle';
 import Home from '@/pages/Home';
 import { ThemeProvider } from 'styled-components';
 import defaultTheme from './theme/theme';
+import ProjectPage from './pages/ProjectPage';
 
 const Navigation = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/project" element={<ProjectPage />} />
       </Routes>
     </BrowserRouter>
   );
