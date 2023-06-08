73c494b05e99374c78db57331a6126224df69df0
Ksechan <rlatpcks77@gmail.com>
2023-06-08 16:52:28 +0900

    feat: add project progress section data

--- src/pages/ProjectPage/ProjectProgress/index.tsx
+++ src/pages/ProjectPage/ProjectProgress/index.tsx
@@ -1,23 +1,69 @@
 import S from './style';
 import CommonS from '../style';
 import FirstSectionImage from '@/assets/images/hanwha_home01.png';
+import { ProjectListProps } from '@/pages/Home/components/PageInformation';
 
-const ProjectProgress = () => {
+const ProjectProgress = ({ item }: ProjectListProps) => {
   return (
-    <>
+    <S.ThirdSectionWrap>
+      <CommonS.SectionNumber marginBottom={60}>0 3</CommonS.SectionNumber>
+      <S.ThirdSectionName>PROGRESS</S.ThirdSectionName>
       <S.ThirdSection>
         <S.ThirdSectionImageWrap>
           <S.ThirdSectionImage src={FirstSectionImage} />
         </S.ThirdSectionImageWrap>
         <S.ThirdSectionNameWrap>
-          <CommonS.SectionNumber marginBottom={60}>0 3</CommonS.SectionNumber>
-          <CommonS.SectionDescription marginBottom={120}>
-            설명설명
-          </CommonS.SectionDescription>
-          <S.ThirdSectionName>PROGRESS</S.ThirdSectionName>
+          <S.ThirdSectionDescription>
+            {item.project?.projectProgressDesc01}
+          </S.ThirdSectionDescription>
+          <S.ThirdSectionName marginTop={40}>
+            {item.project?.projectProgressDescTitle01}
+          </S.ThirdSectionName>
         </S.ThirdSectionNameWrap>
       </S.ThirdSection>
-    </>
+
+      <S.ThirdSection right>
+        <S.ThirdSectionNameWrap>
+          <S.ThirdSectionDescription marginRight={80}>
+            {item.project?.projectProgressDesc02}
+          </S.ThirdSectionDescription>
+          <S.ThirdSectionName marginTop={60}>
+            {item.project?.projectProgressDescTitle02}
+          </S.ThirdSectionName>
+        </S.ThirdSectionNameWrap>
+        <S.ThirdSectionImageWrap>
+          <S.ThirdSectionImage src={FirstSectionImage} />
+        </S.ThirdSectionImageWrap>
+      </S.ThirdSection>
+
+      <S.ThirdSection>
+        <S.ThirdSectionImageWrap>
+          <S.ThirdSectionImage src={FirstSectionImage} />
+        </S.ThirdSectionImageWrap>
+        <S.ThirdSectionNameWrap>
+          <S.ThirdSectionName marginBottom={60} marginLeft={200}>
+            {item.project?.projectProgressDescTitle03}
+          </S.ThirdSectionName>
+          <S.ThirdSectionDescription>
+            {item.project?.projectProgressDesc03}
+          </S.ThirdSectionDescription>
+        </S.ThirdSectionNameWrap>
+      </S.ThirdSection>
+
+      <S.ThirdSection right>
+        <S.ThirdSectionNameWrap>
+          <S.ThirdSectionName marginBottom={60}>
+            {item.project?.projectProgressDescTitle04}
+          </S.ThirdSectionName>
+          <S.ThirdSectionDescription marginRight={40}>
+            {item.project?.projectProgressDesc04}
+          </S.ThirdSectionDescription>
+        </S.ThirdSectionNameWrap>
+        <S.ThirdSectionImageWrap>
+          <S.ThirdSectionImage src={FirstSectionImage} />
+        </S.ThirdSectionImageWrap>
+      </S.ThirdSection>
+    </S.ThirdSectionWrap>
   );
 };
 
--- src/pages/ProjectPage/ProjectProgress/style.tsx
+++ src/pages/ProjectPage/ProjectProgress/style.tsx
@@ -2,23 +2,38 @@ import styled from 'styled-components';
 import { visibleAnimation } from '../style';
 
 const S = {
-  ThirdSection: styled.section`
+  ThirdSectionWrap: styled.section`
     width: 100%;
-    height: 100vh;
+    height: auto;
+    padding-top: 160px;
+  `,
+
+  ThirdSection: styled.div<{ right?: boolean }>`
+    width: 100%;
     animation: ${visibleAnimation} 0.5s;
     display: flex;
-    padding-top: 160px;
+    justify-content: ${({ right }) => (right ? 'flex-end' : 'flex-start')};
+    padding-top: 100px;
   `,
 
   ThirdSectionNameWrap: styled.div`
     padding: 160px 0 0 30px;
   `,
 
-  ThirdSectionName: styled.h3`
+  ThirdSectionName: styled.h3<{
+    marginTop?: number;
+    marginRight?: number;
+    marginBottom?: number;
+    marginLeft?: number;
+  }>`
     font-size: 6rem;
     white-space: pre-wrap;
     color: ${({ theme }) => theme.color.primary};
     font-family: 'Roboto Condensed', sans-serif;
+    margin-top: ${({ marginTop }) => (marginTop ? marginTop : 0)}px;
+    margin-right: ${({ marginRight }) => (marginRight ? marginRight : 0)}px;
+    margin-bottom: ${({ marginBottom }) => (marginBottom ? marginBottom : 0)}px;
+    margin-left: ${({ marginLeft }) => (marginLeft ? marginLeft : 0)}px;
   `,
 
   ThirdSectionImageWrap: styled.div`
@@ -30,6 +45,14 @@ const S = {
 
   ThirdSectionImage: styled.img`
     width: 400px;
+  `,
+
+  ThirdSectionDescription: styled.p<{ marginRight?: number }>`
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme.color.lightGray};
+    max-width: 800px;
+    line-height: 40px;
+    margin-right: ${({ marginRight }) => (marginRight ? marginRight : 0)}px;
   `,
 };
 
