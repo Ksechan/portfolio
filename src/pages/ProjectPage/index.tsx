9d711362f77819b9b13a9a9b8a9ada5e0610fdfc
Ksechan <rlatpcks77@gmail.com>
2023-06-08 14:31:52 +0900

    feat: add email, github link

--- src/pages/ProjectPage/index.tsx
+++ src/pages/ProjectPage/index.tsx
@@ -3,8 +3,11 @@ import IntroMessage from './components/IntroMessage';
 import S from './style';
 import ProjectName from './ProjectName';
 import ProjectConcept from './ProjectConcept';
+import ProjectProgress from './ProjectProgress';
+import { Link, useNavigate } from 'react-router-dom';
 
 const ProjectPage = () => {
+  const navigation = useNavigate();
   const [animation, setAnimation] = useState(true);
 
   useEffect(() => {
@@ -12,6 +15,15 @@ const ProjectPage = () => {
       setAnimation(false);
     }, 2500);
   }, []);
+
+  const sendEmailHandler = () => {
+    return (window.location.href = 'mailto:rlatpcks77@gmail.com');
+  };
+
+  const goHomeButtonHandler = () => {
+    navigation(-1);
+  };
+
   return (
     <S.Container>
       {animation ? (
@@ -22,6 +34,35 @@ const ProjectPage = () => {
           <ProjectName />
           {/* project concept section */}
           <ProjectConcept />
+          {/* project progress section */}
+          <ProjectProgress />
+          <S.FinishSection>
+            <S.FinishSectionTitle>THANK YOU FOR VIEWING</S.FinishSectionTitle>
+            <S.FinishSectionContact>
+              phone: 010-3356-9426
+            </S.FinishSectionContact>
+            <Link
+              to="javascript:void(0)"
+              onClick={sendEmailHandler}
+              style={{ textDecoration: 'none' }}
+            >
+              <S.FinishSectionContact>
+                email: rlatpcks77@gmail.com
+              </S.FinishSectionContact>
+            </Link>
+            <Link
+              to="https://github.com/Ksechan"
+              target="_blank"
+              style={{ textDecoration: 'none' }}
+            >
+              <S.FinishSectionContact>
+                github: https://github.com/Ksechan
+              </S.FinishSectionContact>
+            </Link>
+            <S.FinishSectionButton onClick={goHomeButtonHandler}>
+              SECHAN
+            </S.FinishSectionButton>
+          </S.FinishSection>
         </>
       )}
     </S.Container>
--- src/pages/ProjectPage/style.tsx
+++ src/pages/ProjectPage/style.tsx
@@ -47,6 +47,51 @@ const CommonS = {
     margin-bottom: ${({ marginBottom }) => (marginBottom ? marginBottom : 0)}px;
     margin-left: ${({ marginLeft }) => (marginLeft ? marginLeft : 0)}px;
   `,
+  // finish section
+  FinishSection: styled.section`
+    width: 100%;
+    height: 100vh;
+    animation: ${visibleAnimation} 0.5s;
+    padding: 160px 0 160px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `,
+
+  FinishSectionTitle: styled.h3`
+    font-size: 6rem;
+    color: ${({ theme }) => theme.color.primary};
+    font-family: 'Roboto Condensed', sans-serif;
+    margin-bottom: 60px;
+  `,
+
+  FinishSectionContact: styled.p`
+    font-size: 2.25rem;
+    color: ${({ theme }) => theme.color.lightGray};
+    margin-bottom: 40px;
+  `,
+
+  FinishSectionContactEmail: styled.link`
+    font-size: 2.25rem;
+    color: ${({ theme }) => theme.color.lightGray};
+    margin-bottom: 30px;
+  `,
+
+  FinishSectionButton: styled.button`
+    background-color: transparent;
+    color: ${({ theme }) => theme.color.primary};
+    font-size: 1.75rem;
+    margin-top: 48px;
+    cursor: pointer;
+    transition: 0.3s;
+    border-radius: 8px;
+    border: none;
+    padding: 8px 16px;
+    :hover {
+      background-color: ${({ theme }) => theme.color.gray};
+    }
+  `,
 };
 
 export default CommonS;
