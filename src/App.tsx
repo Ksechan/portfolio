e4e35872d920dbb1724cad059a7184e8841ba263
Ksechan <rlatpcks77@gmail.com>
2023-06-20 17:46:08 +0900

    feat: add mypage router

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './components/GlobalStyle';
 import Home from '@/pages/Home';
 import { ThemeProvider } from 'styled-components';
-import defaultTheme from './theme/theme';
-import ProjectPage from './pages/ProjectPage';
+import defaultTheme from '@/theme/theme';
+import ProjectPage from '@/pages/ProjectPage';
+import MyPage from '@/pages/MyPage';
 
 const Navigation = () => {
   return (
@@ -11,6 +12,7 @@ const Navigation = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project" element={<ProjectPage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
