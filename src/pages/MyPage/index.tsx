39aad0b13cfb638740423646eb2959a2e63a0dfa
Ksechan <rlatpcks77@gmail.com>
2023-06-21 18:00:37 +0900

    feat: add mypage description

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,9 +1,117 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { projectList } from '@/data';
 import S from './style';
 
 const MyPage = () => {
+  const navigate = useNavigate();
+  const sendEmailHandler = () => {
+    return (window.location.href = 'mailto:rlatpcks77@gmail.com');
+  };
+
   return (
     <S.Container>
-      <S.Title>SECHAN</S.Title>
+      <S.SectionTitle>김세찬</S.SectionTitle>
+      <S.ListWrap>
+        <p>* 저는</p>
+        <S.ListTextWrap>
+          <S.ListText>변화를 추구하는</S.ListText>
+          <S.ListText>사람을 좋아하는</S.ListText>
+          <S.ListText>개발문화를 고민하는</S.ListText>
+        </S.ListTextWrap>
+        <p>엔지니어입니다.</p>
+      </S.ListWrap>
+
+      <S.DescText>{`안녕하세요. 2년차 프론트엔드 개발자 김세찬입니다.\n스타트업 '인베스티'애서 약 1년 6개월간 frontend-manager로 근무하였습니다.\n주로 디자이너와 소통하며 문제를 찾고 해결하려고 노력하였으며, 효율적인 협업을 위한 개발역량을 쌓아왔습니다.\n\n아직은 많이 부족하다고 생각하여 다양한 컨텐츠에서 개발 관련 정보 및 강의 시청을 하고 있으며,\n많은 동료들과 함께 성장할 수 있는 좋은 경험을 쌓고 싶습니다.`}</S.DescText>
+
+      <S.Table>
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <strong>GitHub</strong>
+            </td>
+            <td>
+              <Link
+                to="https://github.com/Ksechan"
+                target="_blank"
+                style={{ textDecoration: 'none' }}
+              >
+                <span>https://github.com/Ksechan</span>
+              </Link>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <strong>Contact</strong>
+            </td>
+            <td>010-3356-9426</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>email</strong>
+            </td>
+            <td>
+              <Link
+                onClick={sendEmailHandler}
+                style={{ textDecoration: 'none' }}
+                to={''}
+              >
+                <span>rlatpcks77@gmail.com</span>
+              </Link>
+            </td>
+          </tr>
+        </tbody>
+      </S.Table>
+
+      <S.SectionTitle>Experiences</S.SectionTitle>
+      {projectList.map((item) => {
+        if (item.id > 0) {
+          return (
+            <>
+              <S.ProjectTitle
+                onClick={() => {
+                  navigate('/project', { state: { item } });
+                }}
+              >
+                * {item.project?.projectTitle}
+              </S.ProjectTitle>
+              <S.Table>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <strong>period</strong>
+                    </td>
+                    <td>{item.period}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>project</strong>
+                    </td>
+                    <td>{item.title}</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <strong>tech</strong>
+                    </td>
+                    <td>{item.tech}</td>
+                  </tr>
+                </tbody>
+              </S.Table>
+            </>
+          );
+        }
+      })}
     </S.Container>
   );
 };
--- src/pages/MyPage/style.tsx
+++ src/pages/MyPage/style.tsx
@@ -2,13 +2,92 @@ import styled from 'styled-components';
 
 const S = {
   Container: styled.div`
-    width: 100%;
-    height: 100vh;
-    padding: 40px;
+    max-width: 46.5rem;
+    padding: 0.8125rem 1.21875rem 2.4375rem 1.21875rem;
+    margin: 0 auto;
   `,
 
-  Title: styled.h3`
+  SectionTitle: styled.h3`
+    font-size: 3rem;
+    font-family: 'Roboto Condensed', sans-serif;
+    font-weight: bold;
     color: ${({ theme }) => theme.color.primary};
+    border-left: 6px solid ${({ theme }) => theme.color.primary};
+    padding-left: 30px;
+    margin: 2em 0 1.5em;
+  `,
+
+  Title: styled.h1`
+    font-size: 3rem;
+    color: #ffeee4;
+    font-weight: 500;
+    margin-bottom: 0.5em;
+  `,
+
+  ListWrap: styled.div`
+    display: flex;
+    align-items: center;
+    margin: 2em 0 3em 0;
+
+    p {
+      font-size: 1.5rem;
+      color: #ffeee4;
+    }
+  `,
+
+  ListTitle: styled.p`
+    font-size: 2rem;
+    color: #ffeee4;
+  `,
+
+  ListTextWrap: styled.ul`
+    margin: 0 8px;
+    text-align: center;
+  `,
+
+  ListText: styled.li`
+    font-size: 1.25rem;
+    color: ${({ theme }) => theme.color.primary};
+    line-height: 2em;
+  `,
+
+  Table: styled.table`
+    width: 100%;
+    margin-top: 3.5em;
+
+    span {
+      color: ${({ theme }) => theme.color.lightGray};
+    }
+
+    td:first-child {
+      text-align: right;
+      padding-right: 30px;
+      color: #ffeee4;
+    }
+
+    td {
+      border-top: 1px solid ${({ theme }) => theme.color.gray};
+      border-bottom: 1px solid ${({ theme }) => theme.color.gray};
+      padding: 16px 10px;
+      font-size: 1.25rem;
+      color: ${({ theme }) => theme.color.lightGray};
+    }
+  `,
+
+  DescText: styled.p`
+    font-size: 1rem;
+    color: #ffeee4;
+    line-height: 2rem;
+    white-space: pre-wrap;
+  `,
+
+  ProjectTitle: styled.button`
+    font-size: 2rem;
+    color: ${({ theme }) => theme.color.primary};
+    margin-top: 3em;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
   `,
 };
 
